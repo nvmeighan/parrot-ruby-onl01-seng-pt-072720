c16cd3d2cebabe70c319e8f67b07a0c8aa4efa23
smiling-modem-1057 <nicole.v.meighan@gmail.com>
2020-07-29 22:20:05 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def '#parrot' (name = "Squawk!")
-"Squawk!"
-puts "Squawk!"
+ puts Squawk
+Squawk
 end
-parrot
+#parrot
